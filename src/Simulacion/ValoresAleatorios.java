c2c35523bcf18c2b8f19c5364bd89b709c727f4f
Kevin Delgado <kefdelgado@gmail.com>
2018-06-08 18:07:14 -0600

    valores aleatorios

--- src/Simulacion/ValoresAleatorios.java
+++ src/Simulacion/ValoresAleatorios.java
@@ -4,27 +4,38 @@ import Dominio.Enumeraciones.TipoConsulta;
 
 import java.util.EnumMap;
 import java.util.Map;
+import java.util.Random;
 
 public class ValoresAleatorios {
     private static final Map<TipoConsulta, Double> distribucionTipoConsulta = new EnumMap<>(TipoConsulta.class);
 
-    public static double generarValorDistribucionNormal(int varianza, int media) {
-        // TODO
-        return 0;
+    static {
+        distribucionTipoConsulta.put(TipoConsulta.SELECT, 0.30);
+        distribucionTipoConsulta.put(TipoConsulta.UPDATE, 0.55);
+        distribucionTipoConsulta.put(TipoConsulta.JOIN, 0.90);
+        distribucionTipoConsulta.put(TipoConsulta.DDL, (double) 1);
+    }
+
+    private static Random rand = new Random(12345);
+
+    public static double generarValorDistribucionNormal(int desviacionEstandar, int media) {
+        return rand.nextGaussian() * media + desviacionEstandar;
     }
 
     public static double generarValorDistribucionUniforme(int inicioIntervalo, int finalIntervalo) {
-        // TODO
-        return 0;
+        return (finalIntervalo - inicioIntervalo) * rand.nextDouble() + inicioIntervalo;
     }
 
     public static double generarValorDistibucionExponencial(int lambda) {
-        // TODO
-        return 0;
+        return Math.log(rand.nextDouble()) / (-lambda);
     }
 
     public static TipoConsulta generarTipoConsulta() {
-        // TODO
+        double random = rand.nextDouble();
+        for (Map.Entry<TipoConsulta, Double> entrada :
+                distribucionTipoConsulta.entrySet()) {
+            if (random <= entrada.getValue()) return entrada.getKey();
+        }
         return TipoConsulta.SELECT;
     }
 }
