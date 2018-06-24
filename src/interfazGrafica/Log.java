0e9b08f8eeda462828e42ec8191ca119057ae148
Unknown <mata.ecci@gmail.com>
2018-06-23 19:03:37 -0600

    Files, sin prueba

--- src/interfazGrafica/Log.java
+++ src/interfazGrafica/Log.java
@@ -3,19 +3,48 @@ package interfazGrafica;
 import simulacion.estadisticas.DatosParciales;
 import simulacion.estadisticas.ResultadosFinales;
 
+import java.nio.file.*;
+import java.io.IOException;
+
 public class Log {
-    private static String rutaArchivo = "logSimulacion";
+    private static String rutaArchivo = "/logSimulacion.txt";
 
     Log() {
+        Path path = Paths.get(rutaArchivo);
         // TODO: Abrir archivo
     }
 
     public void escribirDatosParciales(DatosParciales datosParciales) {
-        // TODO
+       Path path = Paths.get(rutaArchivo);
+
+       String datosParcialesFormato =
+               "Reloj: " + datosParciales.reloj + "\n" +
+               "Conexiones Completadas: " + datosParciales.numeroConexionesCompletadas + "\n" +
+               "Conexiones Descartadas: " + datosParciales.numeroConexionesDescartadas + "\n" +
+               "Conexiones Expiradas" + datosParciales.numeroConexionesExpiradas + "\n";
+
+        try {
+            Files.write(path, datosParcialesFormato.getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    public void escribirResultadosFinales(ResultadosFinales resultadosFinales) {
-        // TODO
+    public void escribirResultadosFinales(ResultadosFinales resultadosFinales) throws IOException {
+        Path path = Paths.get(rutaArchivo);
+
+        String resultadosFinalesFormato =
+                "Conexiones Completadas: " + resultadosFinales.numeroConexionesCompletadas + "\n" +
+                "Conexiones Descartadas: " + resultadosFinales.numeroConexionesDescartadas + "\n" +
+                "Conexiones Expiradas: " + resultadosFinales.numeroConexionesExpiradas + "\n" +
+                "Promedio de Vida Conexión: " + resultadosFinales.tiempoPromedioVidaConexion + "ms" + "\n";
+
+        try {
+        Files.write(path, resultadosFinalesFormato.getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void cerrarArchivo() {
